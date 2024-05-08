719570572020b0b16c244ee9429f720aa28c9ab8
NormalDuck <1juhuang2@gmail.com>
2024-05-07 17:47:16 -0700

    Different approach again

--- src/init.lua
+++ src/init.lua
@@ -2,15 +2,16 @@ local RunService = game:GetService("RunService")
 local PublicTypes = require(script.PublicTypes)
 
 if RunService:IsServer() then
-	return require(
-			script.DialogueServer
-		) :: { DialogueClient: PublicTypes.DialogueClient, DialogueServer: PublicTypes.DialogueServer }
+	return {
+		DialogueServer = require(script.DialogueServer),
+	} :: { DialogueClient: PublicTypes.DialogueClient, DialogueServer: PublicTypes.DialogueServer }
 else
 	local DialogueServer = script:FindFirstChild("DialogueServer")
 	if DialogueServer and RunService:IsRunning() then
 		DialogueServer:Destroy()
 	end
-	return require(
-			script.DialogueClient
-		) :: { DialogueClient: PublicTypes.DialogueClient, DialogueServer: PublicTypes.DialogueServer }
+
+	return {
+		DialogueClient = require(script.DialogueClient),
+	} :: { DialogueClient: PublicTypes.DialogueClient, DialogueServer: PublicTypes.DialogueServer }
 end
